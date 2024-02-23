a6b12bf9b22a45e4506aca2d87ca269b8bec7be8
Sauerrr <128698531+Sauerrr@users.noreply.github.com>
2024-02-23 12:01:55 -0300

    Add files via upload

---
+++ aula 01/string.js
@@ -0,0 +1,12 @@
+let nome = 'Vitor';
+let endereco = "Marechal C. Rondon";
+let cachorro1 = 'Pipoca';
+let cachorro2 = 'Simba';
+let cachorro3 = "Magnus";
+
+console.log(`O nome do cachorro1 é ${cachorro1}`)
+console.log(`O nome do cachorro2 é ${cachorro2}`)
+console.log(`O nome do cachorro3 é ${cachorro3}`)
+console.log(`O nome do mais lindo do mundo é ${nome}`)
+console.log(`O endereço do Vitor lindo é ${endereco}`)
+console.log(`O endereço do Vitor lindo é ${endereco} `)
