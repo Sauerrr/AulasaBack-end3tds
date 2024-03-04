f995c0602bd80121f87975139048c98bd2e4dc19
Sauerrr <128698531+Sauerrr@users.noreply.github.com>
2024-03-04 11:35:11 -0300

    Add files via upload

---
+++ aula3/idade.js
@@ -0,0 +1,12 @@
+numero = 14
+
+if (numero == 0){
+    console.log("Número neutro ");
+}else if (numero > 0){
+    console.log("Número positivo")
+}else if (numero < 0){
+    console.log("Numero negativo")
+}
+else {
+    console.log("Entrada incorreta")
+}
---
+++ aula3/operadores.js
@@ -0,0 +1,30 @@
+let soma1 = 5.000 + 5.000;
+let soma2 = 10.000 + 5.000; 
+let soma3 = 15.000 + 0.350; 
+
+let subtracao1 = -50 - 50; 
+let subtracao2 = -100 - 100; 
+let subtracao3 = -200 - 200;
+let subtracao4 = -400 - 100; 
+let subtracao5 = -500 - 67; 
+
+let multiplicacao1 = 490 * 2;
+let multiplicacao2 = 980 * 1;
+
+let divisao1 = 168/2;
+let divisao2 = 84/2;
+let divisao3 = 42/2;
+
+let restoDivisao1 = 3000 % 90;
+let restoDivisao2 = 90 % 40;
+let restoDivisao3 = 40 % 30;
+let restoDivisao4 = 30 % 8; 
+
+
+console.log( soma3.toFixed(3)) 
+console.log( subtracao5) 
+console.log( multiplicacao2)
+console.log(divisao3)
+console.log(restoDivisao4)
+console.log (restoDivisao1 + restoDivisao2 + restoDivisao3 + restoDivisao4)
+
